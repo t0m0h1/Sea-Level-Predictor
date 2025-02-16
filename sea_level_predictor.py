b1c765b69acdc9890e9ea36806b4b9f869328373
t0m0h1 <thomashathawayhogg@gmail.com>
2025-02-16 22:49:07 +0000

    lines of best fit

--- sea_level_predictor.py
+++ sea_level_predictor.py
@@ -9,16 +9,21 @@ def draw_plot():
     # Create scatter plot
     plt.scatter(data['Year'], data['CSIRO Adjusted Sea Level'])
 
+    # Create lines of best fit
+    x = data['Year']
+    y = data['CSIRO Adjusted Sea Level']
+    res = linregress(x, y)
 
-    # Create first line of best fit
+    x_pred = pd.Series(range(1880, 2051))
+    y_pred = res.intercept + res.slope * x_pred
+    plt.plot(x_pred, y_pred, 'r')
 
-
-    # Create second line of best fit
-
+    x_second = pd.Series(range(2000, 2051))
+    y_second = res.intercept + res.slope * x_second
+    plt.plot(x_second, y_second, 'g')
 
     # Add labels and title
 
-    
     # Save plot and return data for testing (DO NOT MODIFY)
     plt.savefig('sea_level_plot.png')
     return plt.gca()
