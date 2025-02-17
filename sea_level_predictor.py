bf4da6ebfd759e6002877cdee400fb161631a3e5
t0m0h1 <thomashathawayhogg@gmail.com>
2025-02-17 18:44:03 +0000

    fix mismatch in dates

--- sea_level_predictor.py
+++ sea_level_predictor.py
@@ -9,23 +9,25 @@ def draw_plot():
     # Create scatter plot
     plt.scatter(data['Year'], data['CSIRO Adjusted Sea Level'])
 
-    # Create lines of best fit
-    x = data['Year']
-    y = data['CSIRO Adjusted Sea Level']
-    res = linregress(x, y)
+    # First Line of Best Fit (Using All Data)
+    res = linregress(data['Year'], data['CSIRO Adjusted Sea Level'])
+    x_pred_1 = pd.Series(range(1880, 2051))
+    y_pred_1 = res.intercept + res.slope * x_pred_1
+    plt.plot(x_pred_1, y_pred_1, 'r', label="Best Fit (1880-2050)")
 
-    x_pred = pd.Series(range(1880, 2051))
-    y_pred = res.intercept + res.slope * x_pred
-    plt.plot(x_pred, y_pred, 'r')
+    # Second Line of Best Fit (Using Only Data From 2000 Onward)
+    data_recent = data[data['Year'] >= 2000]  # Ensure only data from 2000 onward is used
+    res_recent = linregress(data_recent['Year'], data_recent['CSIRO Adjusted Sea Level'])
 
-    x_second = pd.Series(range(2000, 2051))
-    y_second = res.intercept + res.slope * x_second
-    plt.plot(x_second, y_second, 'g')
+    x_pred_2 = pd.Series(range(2000, 2051))  # Predict from 2000 to 2050
+    y_pred_2 = res_recent.intercept + res_recent.slope * x_pred_2
+    plt.plot(x_pred_2, y_pred_2, 'g', label="Best Fit (2000-2050)")
 
     # Add labels and title
     plt.xlabel('Year')
     plt.ylabel('Sea Level (inches)')
     plt.title('Rise in Sea Level')
+    plt.legend()
 
     # Save plot and return data for testing (DO NOT MODIFY)
     plt.savefig('sea_level_plot.png')
